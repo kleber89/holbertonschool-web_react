8b7f34c941c6a513a4cb66af267fc6712c3ff72b
kleber89 <benkosisackson@gmail.com>
2025-02-02 10:45:15 -0500

    changes in code

--- TypeScript/task3/js/rud.d.ts
+++
@@ -1,5 +0,0 @@
-import { RowElement, RowID } from "./interface";
-
-declare function insertRow(row: RowElement): RowID;
-declare function deleteRow(rowId: RowID): void;
-declare function updateRow(rowId: RowID, row: RowElement): RowID;
